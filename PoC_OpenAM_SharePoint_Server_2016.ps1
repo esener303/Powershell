972e9c8eba7556244a29b746efc9c059b20878be
Michał Demczyszak <30467629+esener303@users.noreply.github.com>
2021-02-15 18:35:09 +0100

    Update PoC_OpenAM_SharePoint_Server_2016.ps1

--- PoC_OpenAM_SharePoint_Server_2016.ps1
+++ PoC_OpenAM_SharePoint_Server_2016.ps1
@@ -4,9 +4,9 @@ $cert = New-Object system.security.cryptography.x509certificates.x509certificate
 
 $map = New-SPClaimTypeMapping "http://schemas.xmlsoap.org/claims/EmailAddress" -IncomingClaimTypeDisplayName "EmailAddress" -LocalClaimType "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"
 
-$realm = "https://poc.dev.pl:443/"
+$realm = "https://poc.dev.pl:443/" #sharepoiont public endpoint url
 
-$signinurl = "https://testopenam.ecm365.pl:8443/openam/WSFederationServlet/metaAlias/openam-wsfed-idp"
+$signinurl = "https://testopenam.ecm365.pl:8443/openam/WSFederationServlet/metaAlias/openam-wsfed-idp" #openam endpoint url
 
 $idp = New-SPTrustedIdentityTokenIssuer -Name "OpenAM Federation" -Description "OpenAM 9.5.3" -Realm $realm -SignInUrl $signinurl -ImportTrustCertificate $cert -ClaimsMappings $map -IdentifierClaim $map.InputClaimType
 
